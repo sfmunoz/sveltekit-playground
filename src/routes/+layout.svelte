d56811a4c77cf1ffdf9938503abfe294fe253dc3
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-18 18:03:14 +0000

    flowbite-svelte footer used to render the bottom of the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,12 @@
   import favicon from "$lib/assets/favicon.svg";
   import { resolve } from "$app/paths";
   import { Navbar, NavBrand, NavUl, NavLi } from "flowbite-svelte";
+  import {
+    Footer,
+    FooterBrand,
+    FooterLinkGroup,
+    FooterLink,
+  } from "flowbite-svelte";
 
   let { children } = $props();
 </script>
@@ -35,3 +41,35 @@
     >
   </p>
 </footer>
+
+<Footer footerType="default">
+  <div class="mx-auto max-w-screen-xl text-center">
+    <FooterBrand
+      href={resolve("/")}
+      alt="SvelteKit playground"
+      name="SvelteKit playground"
+      aClass="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
+    />
+    <p class="my-6 text-gray-500 dark:text-gray-400">
+      Simple project to learn and play with SvelteKit
+    </p>
+    <FooterLinkGroup
+      class="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white"
+    >
+      <FooterLink aClass="mr-4 hover:underline md:mr-6" href={resolve("/")}
+        >Home</FooterLink
+      >
+      <FooterLink aClass="mr-4 hover:underline md:mr-6" href={resolve("/posts")}
+        >Posts</FooterLink
+      >
+      <FooterLink
+        aClass="mr-4 hover:underline md:mr-6"
+        href="https://github.com/sfmunoz">Github</FooterLink
+      >
+    </FooterLinkGroup>
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      © 2025 <a href="https://sfmunoz.com/" class="hover:underline">sfmunoz</a
+      >. All Rights Reserved.
+    </span>
+  </div>
+</Footer>
