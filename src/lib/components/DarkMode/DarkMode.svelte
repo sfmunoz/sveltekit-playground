ad00a3e9babd385a5bfa3fa9a3a5b6a7f44e43b0
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-20 17:06:25 +0000

    DarkMode adapted to work with flowbite-svelte

--- src/lib/components/DarkMode/DarkMode.svelte
+++ src/lib/components/DarkMode/DarkMode.svelte
@@ -1,40 +1,29 @@
 <script lang="ts">
-  import { Sun, Moon, Monitor, CircleQuestionMark } from "@lucide/svelte";
-  import appCss from "@/app.css?raw";
-  let lightDT = $state("light");
-  let darkDT = $state("dark");
-  // themes: light --default, dark --prefersdark;
-  // themes: lemonade --default, luxury --prefersdark;
-  // themes: autumn --default, business --prefersdark, pastel, garden;
-  const re = /^\s*themes:\s*(.+)\s+\--default\s*,\s*(.+)\s+--prefersdark[,;]/;
-  const lines = appCss.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    const m = re.exec(lines[i]);
-    if (!m) continue;
-    lightDT = m[1];
-    darkDT = m[2];
-    break;
-  }
-  type UsrTheme = "light" | "dark" | "system";
-  type SysTheme = "light" | "dark" | "unknown";
-  const t: string | null = localStorage.getItem("theme");
-  let usrTheme: UsrTheme = $state(t === "light" || t === "dark" ? t : "system");
+  import { browser } from "$app/environment";
+  import Button from "flowbite-svelte/Button.svelte";
+  import SunSolid from "flowbite-svelte-icons/SunSolid.svelte";
+  import MoonSolid from "flowbite-svelte-icons/MoonSolid.svelte";
+  import DesktopPcOutline from "flowbite-svelte-icons/DesktopPcOutline.svelte";
+  import QuestionCircleOutline from "flowbite-svelte-icons/QuestionCircleOutline.svelte";
+  type UsrChoice = "light" | "dark" | "system";
+  type SysChoice = "light" | "dark" | "unknown";
+  const t: string | null = browser ? localStorage.getItem("theme") : null;
+  let usrChoice: UsrChoice = $state(
+    t === "light" || t === "dark" ? t : "system"
+  );
   let lightMedia: boolean = $state(false);
   let darkMedia: boolean = $state(false);
-  let sysTheme: SysTheme = $derived(
+  let sysChoice: SysChoice = $derived(
     lightMedia === darkMedia ? "unknown" : darkMedia ? "dark" : "light"
   );
-  let dataTheme: string = $derived(
-    usrTheme === "light"
-      ? lightDT
-      : usrTheme === "dark"
-        ? darkDT
-        : sysTheme === "dark"
-          ? darkDT
-          : lightDT
+  let lightMode: boolean = $derived(
+    usrChoice === "light" || (usrChoice !== "dark" && sysChoice !== "dark")
   );
-  $effect(() => localStorage.setItem("theme", usrTheme));
-  $effect(() => document.documentElement.setAttribute("data-theme", dataTheme));
+  $effect(() => localStorage.setItem("theme", usrChoice));
+  $effect(() => {
+    if (lightMode) document.body.classList.remove("dark");
+    else document.body.classList.add("dark");
+  });
   $effect(() => {
     const m = window.matchMedia("(prefers-color-scheme: light)");
     lightMedia = m.matches;
@@ -53,38 +42,43 @@
       m.removeEventListener("change", f);
     };
   });
-  const usrThemes: UsrTheme[] = ["light", "system", "dark"];
-  const overC =
-    "absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[58%] scale-60 z-10";
+  const usrThemes: UsrChoice[] = ["light", "system", "dark"];
+  // const overC =
+  //   "absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[58%] scale-60 z-10";
+  const overC = "absolute -translate-x-[60%] -translate-y-[50%]";
 </script>
 
-{#snippet icon(t: UsrTheme)}
+{#snippet icon(t: UsrChoice)}
   {#if t === "light"}
-    <Sun />
+    <SunSolid />
   {:else if t === "dark"}
-    <Moon />
+    <MoonSolid />
   {:else if t === "system"}
-    <Monitor />
-    {#if sysTheme === "light"}
-      <Sun class={overC} />
-    {:else if sysTheme === "dark"}
-      <Moon class={overC} />
+    {#if sysChoice === "light"}
+      <DesktopPcOutline />
+      <SunSolid
+        class="absolute -translate-x-[60%] -translate-y-[50%] scale-80"
+      />
+    {:else if sysChoice === "dark"}
+      <DesktopPcOutline />
+      <MoonSolid
+        class="absolute translate-x-[60%] -translate-y-[50%] scale-80"
+      />
     {:else}
-      <CircleQuestionMark class={overC} />
+      <QuestionCircleOutline class="text-red-700 dark:text-red-300" />
     {/if}
   {/if}
 {/snippet}
 
-<div
-  class="join join-vertical lg:join-horizontal tooltip tooltip-bottom"
-  data-tip="light={lightDT} | dark={darkDT}"
->
+<div class="grid grid-cols-3 ml-4 justify-center items-center gap-1">
   {#each usrThemes as t (t)}
-    <button
+    <Button
       id={t}
-      class="btn btn-xs join-item"
-      disabled={usrTheme === t}
-      onclick={() => (usrTheme = t)}>{@render icon(t)}</button
+      disabled={usrChoice === t}
+      color={usrChoice === t ? "primary" : "secondary"}
+      size="xs"
+      outline
+      onclick={() => (usrChoice = t)}>{@render icon(t)}</Button
     >
   {/each}
 </div>
